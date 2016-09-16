30483bf0bd9226f4050b89533362fae2034e65d2
David Keith <dkeith2@gmu.edu>
2016-09-16 00:49:33 -0400

    Added some js control to login.html (for real this time)

---
+++ app.js
@@ -0,0 +1,3 @@
+/**
+ * Created by david on 9/15/16.
+ */
--- login.js
+++ login.js
@@ -1,3 +1,31 @@
 /**
- * Created by david on 9/13/16.
+ * To control the html login page
  */
+        sessionStorage.isLoggedIn = false;
+
+       $(Document).ready(function() {
+           if (sessionStorage.isLoggedIn === false) {
+               console.log(sessionStorage.isLoggedIn);
+               goToApp();
+           }
+           else {
+                $("#UserLoggedIn").hide();
+           }
+
+           // When the submit button is clicked
+           $("#PasswordSubmit").click(function(){
+               console.log(sessionStorage.isLoggedIn);
+               sessionStorage.isLoggedIn = true;
+               goToApp();
+               console.log(sessionStorage.isLoggedIn);
+           });
+       });
+
+// Redirect user to application
+function goToApp(){
+    $("#LoginForm").hide();
+    $("#UserLoggedIn").show();
+    window.setTimeout(function(){ window.location = "https://david-keith.github.io/GeoInfo/app" },3000);
+
+    console.log("Went to app");
+}
