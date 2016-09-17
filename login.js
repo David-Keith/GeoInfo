6f022f0d4ea287d69ef3b0e5264930efc9f6e824
David Keith <dkeith2@gmu.edu>
2016-09-17 00:18:49 -0400

    Fixed bug in loading new page

--- login.js
+++ login.js
@@ -1,11 +1,11 @@
 /**
  * To control the html login page
  */
-
        $(document).ready(function() {
-           sessionStorage.setItem("isLoggedIn", false);
-           sessionStorage.isLoggedIn = "false";
-
+           if(sessionStorage.getItem("isLoggedIn") == undefined) {
+               sessionStorage.setItem("isLoggedIn", "false");
+           }
+           var loggedIn = "false;"
            // if (sessionStorage.isLoggedIn.valueOf() === "true") {
            // if(sessionStorage.getItem("isLoggedIn"))
            //     console.log("isLoggedIn: " + sessionStorage.isLoggedIn);
@@ -13,13 +13,19 @@
            // }
            // When the submit button is clicked
            $("#PasswordSubmit").click(function(){
-               sessionStorage.setItem("isLoggedIn", true);
+               sessionStorage.setItem("isLoggedIn", "true");
+               loggedIn = "true";
                // goToApp();
            });
 
            var intervalID = setInterval(function() {
-               if (sessionStorage.getItem("isLoggedIn")) {
+               console.log(sessionStorage.getItem("isLoggedIn"));
+               if (sessionStorage.getItem("isLoggedIn") === "true") {
                    goToApp();
+               }
+               if(loggedIn === "true")
+               {
+                   console.log(loggedIn);
                }
            }, 2000);
 
@@ -27,7 +33,7 @@
            function goToApp(){
                $("#LoginForm").hide();
                $("#UserLoggedIn").show();
-               window.setTimeout(function(){ window.location = "https://david-keith.github.io/GeoInfo/app" },5000);
+               window.setTimeout(function(){ window.location = "app.html" },5000);
            }
        });
 
