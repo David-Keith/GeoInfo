47926b68a316a0d211a7ce487f7dd7c50cd959ed
David Keith <dkeith2@gmu.edu>
2016-09-17 00:39:31 -0400

    Added interactivity to the app page.

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
 /**
- * Created by david on 9/15/16.
+ * controls the app page of the website
  */
+
+$(document).ready(function () {
+    if (sessionStorage.getItem("isLoggedIn") == undefined) {
+        sessionStorage.setItem("isLoggedIn", "false");
+    }
+
+    // Run every 2 seconds to check if user is logged in
+    var intervalID = setInterval(function () {
+        console.log(sessionStorage.getItem("isLoggedIn"));
+        if (sessionStorage.getItem("isLoggedIn") === "false") {
+            goToLogin();
+        }
+    }, 2000);
+
+    // Redirect user to login page
+    function goToLogin() {
+        $("#AppMain").hide();
+        $("#UserNotLoggedIn").show();
+
+        // Go to app after 5 seconds
+        window.setTimeout(function () {
+            window.location = "login.html"
+        }, 5000);
+    }
+});
+
+
--- login.js
+++ login.js
@@ -1,39 +1,33 @@
 /**
  * To control the html login page
  */
-       $(document).ready(function() {
-           if(sessionStorage.getItem("isLoggedIn") == undefined) {
-               sessionStorage.setItem("isLoggedIn", "false");
-           }
-           var loggedIn = "false;"
-           // if (sessionStorage.isLoggedIn.valueOf() === "true") {
-           // if(sessionStorage.getItem("isLoggedIn"))
-           //     console.log("isLoggedIn: " + sessionStorage.isLoggedIn);
-           //     goToApp();
-           // }
-           // When the submit button is clicked
-           $("#PasswordSubmit").click(function(){
-               sessionStorage.setItem("isLoggedIn", "true");
-               loggedIn = "true";
-               // goToApp();
-           });
+$(document).ready(function () {
+    if (sessionStorage.getItem("isLoggedIn") == undefined) {
+        sessionStorage.setItem("isLoggedIn", "false");
+    }
+    // When the submit button is clicked
+    $("#PasswordSubmit").click(function () {
+        sessionStorage.setItem("isLoggedIn", "true");
+        // goToApp();
+    });
 
-           var intervalID = setInterval(function() {
-               console.log(sessionStorage.getItem("isLoggedIn"));
-               if (sessionStorage.getItem("isLoggedIn") === "true") {
-                   goToApp();
-               }
-               if(loggedIn === "true")
-               {
-                   console.log(loggedIn);
-               }
-           }, 2000);
+    // Run every 2 seconds to check if user is logged in
+    var intervalID = setInterval(function () {
+        console.log(sessionStorage.getItem("isLoggedIn"));
+        if (sessionStorage.getItem("isLoggedIn") === "true") {
+            goToApp();
+        }
+    }, 2000);
 
-           // Redirect user to application
-           function goToApp(){
-               $("#LoginForm").hide();
-               $("#UserLoggedIn").show();
-               window.setTimeout(function(){ window.location = "app.html" },5000);
-           }
-       });
+    // Redirect user to application
+    function goToApp() {
+        $("#LoginForm").hide();
+        $("#UserLoggedIn").show();
+
+        // Go to app after 5 seconds
+        window.setTimeout(function () {
+            window.location = "app.html"
+        }, 5000);
+    }
+});
 
