03b6897fc5589ec2fe7379992c383fa76077856f
David Keith <dkeith2@gmu.edu>
2016-09-16 23:47:15 -0400

    Got the login redirect working

--- login.js
+++ login.js
@@ -1,31 +1,33 @@
 /**
  * To control the html login page
  */
-        sessionStorage.isLoggedIn = false;
 
-       $(Document).ready(function() {
-           if (sessionStorage.isLoggedIn === false) {
-               console.log(sessionStorage.isLoggedIn);
-               goToApp();
-           }
-           else {
-                $("#UserLoggedIn").hide();
-           }
+       $(document).ready(function() {
+           sessionStorage.setItem("isLoggedIn", false);
+           sessionStorage.isLoggedIn = "false";
 
+           // if (sessionStorage.isLoggedIn.valueOf() === "true") {
+           // if(sessionStorage.getItem("isLoggedIn"))
+           //     console.log("isLoggedIn: " + sessionStorage.isLoggedIn);
+           //     goToApp();
+           // }
            // When the submit button is clicked
            $("#PasswordSubmit").click(function(){
-               console.log(sessionStorage.isLoggedIn);
-               sessionStorage.isLoggedIn = true;
-               goToApp();
-               console.log(sessionStorage.isLoggedIn);
+               sessionStorage.setItem("isLoggedIn", true);
+               // goToApp();
            });
+
+           var intervalID = setInterval(function() {
+               if (sessionStorage.getItem("isLoggedIn")) {
+                   goToApp();
+               }
+           }, 2000);
+
+           // Redirect user to application
+           function goToApp(){
+               $("#LoginForm").hide();
+               $("#UserLoggedIn").show();
+               window.setTimeout(function(){ window.location = "https://david-keith.github.io/GeoInfo/app" },5000);
+           }
        });
 
-// Redirect user to application
-function goToApp(){
-    $("#LoginForm").hide();
-    $("#UserLoggedIn").show();
-    window.setTimeout(function(){ window.location = "https://david-keith.github.io/GeoInfo/app" },3000);
-
-    console.log("Went to app");
-}
