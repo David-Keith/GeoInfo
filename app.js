6343ca1cefb917bfdcc7ad5c976245c06b814ae3
David Keith <dkeith2@gmu.edu>
2016-09-19 01:16:03 -0400

    Fixed bug in app.js

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ $(document).ready(function () {
 
     // Run every 2 seconds to check if user is logged in
     var intervalID = setInterval(function () {
-        console.log(sessionStorage.getItem("isLoggedIn"));
         if (sessionStorage.getItem("isLoggedIn") === "false") {
             goToLogin();
         }
@@ -25,51 +24,50 @@ $(document).ready(function () {
             window.location = "login.html"
         }, 5000);
     }
+    var locationsSaved = 0; //keeps track of how many locations a user has saved
+    var locationCapacity = 3; //users can save up this many locations total
+
+//when a user clicks the button to save a new location, dynamically add the html for it to the list of saved locations
+    $("#save").click(function () {
+        //users can currently save up to 3 locations
+        if (locationsSaved >= locationCapacity) return;
+
+        locationsSaved++;
+        var location = Math.random(); //treat this as an identifier for a mock location
+
+        //add the location to the list with a goto and delete button
+        $(".savedLocations").append(
+            '<div class="savedLocation" data-location=' + location + '><button onClick="goToLocation(this)">Go</button> Saved Location: '
+            + location + '<button class="deleteButton" onclick="deleteItem(this.parentElement)">&#x2716;</button></div>'
+        )
+        //disable the save button when the capacity of saved locations is reached
+        if (locationsSaved == locationCapacity) $('#save').prop('disabled', true);
+    })
+
+//when a user clicks the button to go to a saved location, update the map/forecast
+    function goToLocation(elem) {
+        var updatedLocation = elem.parentElement.getAttribute('data-location');
+        $('#current').html("Saved location: " + updatedLocation);
+
+        //make a simple little animation to mock the actual update of the map and forecast
+        $(".interactive").animate({
+            opacity: 0.25,
+            transform: "translate(4%, 4%)",
+            transition: "0s"
+        })
+            .animate({
+                opacity: 1,
+                transform: "translate(-4%, -4%)",
+                transition: "0s"
+            });
+    }
+
+//when a user clicks the location delete button, remove the location and ensure the save location button is not disabled
+    function deleteItem(divElem) {
+        locationsSaved--;
+        divElem.parentElement.removeChild(divElem);
+        if (locationsSaved < locationCapacity) $('#save').prop('disabled', false);
+    }
 });
 
 
-var locationsSaved = 0; //keeps track of how many locations a user has saved
-var locationCapacity = 3; //users can save up this many locations total
-
-//when a user clicks the button to save a new location, dynamically add the html for it to the list of saved locations
-$('#save').click(function() {
-	//users can currently save up to 3 locations
-  if (locationsSaved >= locationCapacity) return;
-  
-  locationsSaved++;
-  var location = Math.random(); //treat this as an identifier for a mock location
-  
-  //add the location to the list with a goto and delete button
-  $(".savedLocations").append(
-    '<div class="savedLocation" data-location=' + location + '><button onClick="goToLocation(this)">Go</button> Saved Location: '
-    + location + '<button class="deleteButton" onclick="deleteItem(this.parentElement)">&#x2716;</button></div>'
-  )
-  //disable the save button when the capacity of saved locations is reached
-  if (locationsSaved == locationCapacity) $('#save').prop('disabled', true);
-})
-
-//when a user clicks the button to go to a saved location, update the map/forecast
-function goToLocation(elem) {
-  var updatedLocation = elem.parentElement.getAttribute('data-location');
-  $('#current').html("Saved location: " + updatedLocation);
-  
-  //make a simple little animation to mock the actual update of the map and forecast
-  $(".interactive").animate({
-    opacity: 0.25,
-    transform: "translate(4%, 4%)",
-    transition: "0s"
-  })
-  .animate({
-  opacity: 1,
-  transform: "translate(-4%, -4%)",
-  transition: "0s"
-  });
-}
-
-//when a user clicks the location delete button, remove the location and ensure the save location button is not disabled
-function deleteItem(divElem)
-{
-		locationsSaved--;
-    divElem.parentElement.removeChild(divElem);
-    if (locationsSaved < locationCapacity) $('#save').prop('disabled', false);
-}
